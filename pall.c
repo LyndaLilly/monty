cd896816ae6cccc287c037b96917f54b44af36ad
soloking44 <134512745+soloking44@users.noreply.github.com>
2023-12-15 01:12:24 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -3,13 +3,13 @@
 /**
  * print_all - this prints all stack
  * @stk: this is the stack head
- * @num: number of stack
+ * @cnt: number of stack
  * Return: nothing
 */
-void print_all(stack_t **stk, unsigned int num)
+void print_all(stack_t **stk, unsigned int cnt)
 {
 	stack_t *p;
-	(void)num;
+	(void)cnt;
 
 	p = *stk;
 	if (p == NULL)
